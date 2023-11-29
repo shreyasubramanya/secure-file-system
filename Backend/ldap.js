3878aa6258a672bf33a9157cf687a591819906c8
Peyton Andras <96754073+PeytonAndras@users.noreply.github.com>
2023-11-28 22:57:49 -0600

    ldap.js changes so that it waits for user login requests and processes

--- Backend/ldap.js
+++ Backend/ldap.js
@@ -1,103 +1,58 @@
-// // const ldap = require('ldapjs');
-
-// // const client = ldap.createClient({
-// //   url: 'ldap://10.211.55.9', //adjust the URL based on your LDAP server configuration
-// // });
-
-// // client.bind('uid=john,ou=people,dc=example,dc=com', 'admin', (err) => {
-// //   if (err) {
-// //     console.error('LDAP bind failed:', err);
-// //   } else {
-// //     console.log('LDAP bind successful');
-
-// //     //perform LDAP operations here
-// //     /**
-// //  * Authenticate a user against the LDAP server.
-// //  * @param {string} username - The username of the user.
-// //  * @param {string} password - The password of the user.
-// //  * @param {function} callback - A callback function to handle the response.
-// //  */
-// // function authenticateUser(username, password, callback) {
-// //   //adjust the DN (Distinguished Name) based on your LDAP server configuration
-// //   const userDN = `uid=${username},ou=people,dc=example,dc=com`;
-
-// //   client.bind(userDN, password, (err) => {
-// //     if (err) {
-// //       console.error('LDAP authentication failed:', err);
-// //       callback(false); //authentication failed
-// //     } else {
-// //       console.log('LDAP authentication successful');
-// //       callback(true); //authentication successful
-// //       client.unbind();
-// //     }
-// //   });
-// // }
-
-// // //example usage
-// // authenticateUser('john', 'admin', (isAuthenticated) => {
-// //   if (isAuthenticated) {
-// //     console.log('User login successful');
-// //     //handle post-login logic here
-// //   } else {
-// //     console.log('User login failed');
-// //     //handle login failure here
-// //   }
-// // });
-// //   }
-// // });
-
-// ////////
-
-
-// const express = require('express');
 // const ldap = require('ldapjs');
-// const bodyParser = require('body-parser');
 
-// const app = express();
-// app.use(bodyParser.json()); // for parsing application/json
-
-// // LDAP client configuration
 // const client = ldap.createClient({
 //   url: 'ldap://10.211.55.9', //adjust the URL based on your LDAP server configuration
 // });
 
-// // LDAP authentication function
+// client.bind('uid=john,ou=people,dc=example,dc=com', 'admin', (err) => {
+//   if (err) {
+//     console.error('LDAP bind failed:', err);
+//   } else {
+//     console.log('LDAP bind successful');
+
+//     //perform LDAP operations here
+//     /**
+//  * Authenticate a user against the LDAP server.
+//  * @param {string} username - The username of the user.
+//  * @param {string} password - The password of the user.
+//  * @param {function} callback - A callback function to handle the response.
+//  */
 // function authenticateUser(username, password, callback) {
+//   //adjust the DN (Distinguished Name) based on your LDAP server configuration
 //   const userDN = `uid=${username},ou=people,dc=example,dc=com`;
 
 //   client.bind(userDN, password, (err) => {
 //     if (err) {
 //       console.error('LDAP authentication failed:', err);
-//       callback(false);
+//       callback(false); //authentication failed
 //     } else {
 //       console.log('LDAP authentication successful');
-//       callback(true);
+//       callback(true); //authentication successful
 //       client.unbind();
 //     }
 //   });
 // }
 
-// // Login endpoint
-// app.post('/login', (req, res) => {
-//   const { username, password } = req.body;
-
-//   authenticateUser(username, password, (isAuthenticated) => {
-//     if (isAuthenticated) {
-//       res.status(200).send('User login successful');
-//     } else {
-//       res.status(401).send('User login failed');
-//     }
-//   });
+// //example usage
+// authenticateUser('john', 'admin', (isAuthenticated) => {
+//   if (isAuthenticated) {
+//     console.log('User login successful');
+//     //handle post-login logic here
+//   } else {
+//     console.log('User login failed');
+//     //handle login failure here
+//   }
+// });
+//   }
 // });
 
-// // Start the server
-// const PORT = 3000;
-// app.listen(PORT, () => {
-//   console.log(`Server running on http://localhost:${PORT}`);
-// });
+// ////////
+
 const express = require('express');
 const ldap = require('ldapjs');
 const bodyParser = require('body-parser');
+const path = require('path');
+const templatePath = path.join(__dirname, '..Frontend/templates');
 
 const app = express();
 app.use(bodyParser.json()); // for parsing application/json
@@ -141,6 +96,8 @@ app.post('/login', (req, res) => {
     }
   });
 });
+
+app.use(express.static('Frontend'));
 
 // Start the server
 const PORT = 3000;
